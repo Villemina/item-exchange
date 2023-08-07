12896da710e161a0a4da4e242799cd968bb6630f
agnieszka <agnieszka.gos.1987@com>
2023-08-08 01:16:01 +0200

    #1b Home - Navigation

---
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "./_HomeHeader.scss"
+
+
+const HomeHeader = () => {
+    return (
+        <div className="header">
+            <div className="header__top">
+                <ul className="header__top-list">
+                    <li><Link className="nav-link" to="/login">Zaloguj</Link></li>
+                    <li><Link className="nav-link" to="/registration">Załóż konto</Link></li>
+                </ul>
+            </div>
+            <div className="header__bottom">
+                <ul className="header__bottom-list">
+                    <li><Link className="nav-link" to="start">Start</Link></li>
+                    <li><Link className="nav-link" to="what">O co chodzi?</Link></li>
+                    <li><Link className="nav-link" to="about">O nas</Link></li>
+                    <li><Link className="nav-link" to="organizations">Fundacja i organizacje</Link></li>
+                    <li><Link className="nav-link" to="contact">Kontakt</Link></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default HomeHeader;
