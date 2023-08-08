f69050c88b9613f9ba7c870ffb97291ac371545b
agnieszka <agnieszka.gos.1987@com>
2023-08-08 02:45:19 +0200

    #1b Home - Main

--- src/components/Home/HomeMainWrapper/HomeMainWrapper.js
+++ src/components/Home/HomeMainWrapper/HomeMainWrapper.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import {Link} from "react-router-dom";
+import Decoration from './Decoration.svg'
+import HomeHeroImage from "./Home-Hero-Image.jpg"
+import "./_HomeMainWrapper.scss"
 
 const HomeMainWrapper = () => {
     return (
-        <div>
-            <h1>hey</h1>
+        <div className="wrapper" id="start">
+            <img className="wrapper__image" src={HomeHeroImage} alt="Box with Pc Items" />
+            <div className="wrapper__right">
+                <h1 className="wrapper__right-header">
+                    Zacznij pomagać! <br /> Oddaj niechciane rzeczy w zaufane ręce
+                </h1>
+                <img className="wrapper__decoration" src={Decoration} alt="fancy interline" />
+                <div className="wrapper__right-buttons">
+                    <ul>
+                        <li>
+                            <Link className="nav-link" to="/login">
+                                <button>
+                                    Oddaj <br />rzeczy
+                                </button>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="nav-link" to="/login">
+                                <button>
+                                    Zorganizuj <br />zbiórkę
+                                </button>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
