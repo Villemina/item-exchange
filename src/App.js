1d73ab44327835974f8710f6cb46beb25add60b5
agnieszka <agnieszka.gos.1987@com>
2023-08-15 23:46:29 +0200

    home contact Quick fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login/Login";
+import Registration from "./components/Login/Registration/Registration";
+
+
+
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path ='login' element={<Login/>}/>
+                <Route path ='registration' element={<Registration/>}/>
             </Routes>
         </BrowserRouter>
     );
