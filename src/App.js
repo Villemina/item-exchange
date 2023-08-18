c77cafa3a39ed1e274532092a84f89fea88ae3f0
agnieszka <agnieszka.gos.1987@com>
2023-08-18 21:52:14 +0200

    app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home";
 import Login from "./components/Login/Login/Login";
 import Registration from "./components/Login/Registration/Registration";
 import Logout from "./components/Login/Logout/Logout";
+import Give from "./components/Giveout/Give/Give";
 
 
 
@@ -15,6 +16,7 @@ function App() {
                 <Route path ='login' element={<Login/>}/>
                 <Route path ='registration' element={<Registration/>}/>
                 <Route path ='logout' element={<Logout/>}/>
+                <Route path ='oddaj-rzeczy' element={<Give/>}/>
             </Routes>
         </BrowserRouter>
     );
